c3925e2c8e73c253ce96acea519d3630a424e218
allthingsclowd <allthingsclowd@users.noreply.github.com>
2017-07-30 00:51:02 +0100

    Modified form to use reactive style - needs polishing

--- K5-Password-Retrevial/src/app/app.component.ts
+++ K5-Password-Retrevial/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  projects = ['Project A', 'Project B'];
+  servers = ['Server 1', 'Server 2', 'Server 3'];
+  passwordForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.passwordForm = new FormGroup({
+      'loginData': new FormGroup({
+        'user': new FormControl(null, [Validators.required]),
+        'password': new FormControl(null, [Validators.required]),
+        'contract': new FormControl(null, [Validators.required])
+      }),
+      'project': new FormControl(null, [Validators.required]),
+      'server': new FormControl(null, [Validators.required]),
+      'pemkey': new FormControl(null, [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.passwordForm);
+    //this.passwordForm.reset();
+  } 
 }
