0e22699dbbc595a45fa00c776c613274f0b12167
allthingsclowd <allthingsclowd@users.noreply.github.com>
2017-08-08 08:48:57 +0100

    Still not working need type scripted promise with catch demo

--- K5-Password-Retrevial/src/app/services/password-management.service.ts
+++ K5-Password-Retrevial/src/app/services/password-management.service.ts
@@ -3,48 +3,54 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class PasswordManagementService {
-    buf: Uint8Array = new Uint8Array(8);
-    bufView : ArrayBufferView;
-      
-    constructor() { 
-      
+    
+    static getArrayFromString(str:string) {
+        return new TextEncoder('utf-8').encode(str);
     }
 
-    // getCryptoRandom(){
-    //     console.log('Now we should see something');
-    //     this.bufView = this.buf;
-    //     console.log(this.bufView);        
-    //     console.log(this.uint8arrayToStringMethod(this.buf));
-    //     this.bufView = window.crypto.getRandomValues(this.buf);
-    //     console.log(this.bufView);
-    //     console.log(this.uint8arrayToStringMethod(this.buf));
-    //     console.log('And its over');
-    // }
-    
-    // uint8arrayToStringMethod(myUint8Arr: Uint8Array): string {
-    //     return String.fromCharCode.apply(null, myUint8Arr);
-    // }
+    static getRandomValues():ArrayBuffer {
+        var array = new Int32Array(16);
+        window.crypto.getRandomValues(array);
+        return array.buffer;
+    }
+    static throwError(err){
+        throw new Error(err);
+    }
 
-    // decrypt(dataArrayBuffer:ArrayBuffer):Promise<any> {
-    //     return window.crypto.decrypt({name: "RSA-OAEP"}, this.KEY, dataArrayBuffer);
-    // }
+    Crypto;
+    name: string = 'RSA-OAEP';
+    keyUsagesEncryptDecrypt: string[] = ["encrypt", "decrypt"];
+    KEY;
 
-    // decryptPassword(privateKey, data){
-    //   window.crypto.subtle.decrypt(
-    //     {
-    //         name: "RSA-OAEP",
-    //         //label: Uint8Array([...]) //optional
-    //     },
-    //     privateKey, //from generateKey or importKey above
-    //     data //ArrayBuffer of the data
-    //     )
-    //     .then(function(decrypted){
-    //         //returns an ArrayBuffer containing the decrypted data
-    //         console.log(new Uint8Array(decrypted));
-    //     });
-      
-    // }
+    constructor() {
+        this.Crypto = window.crypto.subtle;
+    }
+    getKey():Promise<any> {
+        if(this.KEY){
+            return new Promise((resolve, reject) => {
+                resolve(this.KEY);
+            });
+        }
+        return this.keyGenerate();
+    }
+    keyGenerate():Promise<any> {
+        const keyDetails = {
+            'name': this.name,
+            modulusLength: 2048, //can be 1024, 2048, or 4096
+            publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+            'hash': { name: "SHA-256" }
+        };
 
- 
+        let dfd = new Promise((resolve, reject) => {
+            this.Crypto.generateKey(keyDetails, true, this.keyUsagesEncryptDecrypt).then((key) => {
+                this.KEY = key.publicKey;
+                resolve(key.publicKey);
+            }, Utils.throwError);
+        });
 
+        return dfd;
+    }
+    encrypt(dataArrayBuffer:ArrayBuffer):Promise<any> {
+        return this.Crypto.encrypt({name: "RSA-OAEP"}, this.KEY, dataArrayBuffer);
+    }
 }
