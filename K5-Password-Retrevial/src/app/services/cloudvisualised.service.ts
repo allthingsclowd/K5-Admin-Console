50fd429ebbb075530bc36f518b78ac58d57e248c
allthingsclowd <allthingsclowd@users.noreply.github.com>
2017-10-05 12:44:59 +0100

    wip

--- K5-Password-Retrevial/src/app/services/cloudvisualised.service.ts
+++ K5-Password-Retrevial/src/app/services/cloudvisualised.service.ts
@@ -3,6 +3,24 @@ import { LoadbalancerService } from './loadbalancer.service';
 import { ComputeService } from './compute.service';
 import { Injectable } from '@angular/core';
 
+interface IVisualLink {
+  source: number;
+  target: number;
+  weight: number;
+}
+
+class VisualLink implements IVisualLink {
+  source: number;
+  target: number;
+  weight: number;
+
+  constructor(source: number, target: number, weight?: number) {
+      this.source = source;
+      this.target = target;
+      this.weight = weight || undefined;
+  }
+}
+
 interface IVisualNode {
   id: string;
   name: string;
@@ -31,6 +49,22 @@ class VisualNode implements IVisualNode {
   }
 }
 
+class VisualisationData {
+  nodes: Array<VisualNode>;
+  edges: Array<VisualLink>;
+
+  constructor() {
+    this.nodes = [];
+    this.edges = [];
+
+  }
+
+  addNodes(node: VisualNode, edge: VisualLink): void {
+    this.nodes.push(node);
+    this.edges.push(edge);
+}
+}
+
 @Injectable()
 export class CloudvisualisedService {
   k5Nodes: Array<VisualNode>;
@@ -41,22 +75,23 @@ export class CloudvisualisedService {
   routers: any;
   loadbalancers: any;
 
-  constructor(
-              private computeService: ComputeService,
-              private loadBalancerService: LoadbalancerService,
-              private networkService: NetworkService) {
+  constructor() {}
+              // private computeService: ComputeService,
+              // private loadBalancerService: LoadbalancerService,
+              // private networkService: NetworkService) {
 
-                this.computeService.userServers.subscribe(currentServers => this.servers = currentServers);
-                this.loadBalancerService.userLBaaS.subscribe(currentLbaas => this.loadbalancers = currentLbaas);
-                this.networkService.userPorts.subscribe(currentPorts => this.ports = currentPorts);
-                this.networkService.userSubNetworks.subscribe(currentSubnets => this.subnets = currentSubnets);
-                this.networkService.userNetworks.subscribe(currentNetworks => this.networks = currentNetworks);
-                this.networkService.userRouters.subscribe(currentRouters => this.routers = currentRouters);
-              }
+              //   // this.computeService.userServers.subscribe(currentServers => this.servers = currentServers);
+              //   // this.loadBalancerService.userLBaaS.subscribe(currentLbaas => this.loadbalancers = currentLbaas);
+              //   // this.networkService.userPorts.subscribe(currentPorts => this.ports = currentPorts);
+              //   // this.networkService.userSubNetworks.subscribe(currentSubnets => this.subnets = currentSubnets);
+              //   // this.networkService.userNetworks.subscribe(currentNetworks => this.networks = currentNetworks);
+              //   // this.networkService.userRouters.subscribe(currentRouters => this.routers = currentRouters);
+              // }
 
 
-  getNodes() {
+  getNodes(nodelist: Array<any>) {
     console.log('Visualisation Nodes');
+    let node in nodelist
     console.log(this.ports);
     console.log(this.subnets);
     console.log(this.networks);
