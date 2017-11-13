2aa985f13ad6e98cef2b8d32f5f9748747104540
allthingsclowd <allthingsclowd@users.noreply.github.com>
2017-11-13 22:05:30 +0000

    Need to add the ike form details

--- K5-Password-Retrevial/src/app/ipsecvpn.component.ts
+++ K5-Password-Retrevial/src/app/ipsecvpn.component.ts
@@ -1,3 +1,5 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IdentityService } from './services/identity.service';
 import { Component, OnInit } from '@angular/core';
 import { IpsecvpnService } from './services/ipsecvpn.service';
 
@@ -15,8 +17,18 @@ export class IpsecvpnComponent implements OnInit {
   ikePolicy: any;
   ipsecConnections: any;
   ipsecConnection: any;
+  currentProjectT: any;
+  amendVpnServiceForm: FormGroup;
+  addVpnServiceForm: FormGroup;
+  amendIpsecPolicyForm: FormGroup;
+  addIpsecPolicyForm: FormGroup;
+  amendIkePolicyForm: FormGroup;
+  addIkePolicyForm: FormGroup;
+  amendConnectionForm: FormGroup;
+  addConnectionForm: FormGroup;
 
-  constructor(private ipsecvpnService: IpsecvpnService) {
+  constructor(private ipsecvpnService: IpsecvpnService,
+              private identityService: IdentityService) {
     this.ipsecvpnService.ipsecPolicies.subscribe(policies => this.ipsecPolicies = policies);
     this.ipsecvpnService.ipsecPolicy.subscribe(policy => this.ipsecPolicy = policy);
     this.ipsecvpnService.ikePolicies.subscribe(policies => this.ikePolicies = policies);
@@ -25,9 +37,99 @@ export class IpsecvpnComponent implements OnInit {
     this.ipsecvpnService.vpnService.subscribe(service => this.vpnService = service);
     this.ipsecvpnService.ipsecConnections.subscribe(connections => this.ipsecConnections = connections);
     this.ipsecvpnService.ipsecConnection.subscribe(connection => this.ipsecConnection = connection);
+    this.identityService.userPToken.subscribe(currentProjectToken => this.currentProjectT = currentProjectToken);
   }
 
   ngOnInit() {
+    this.amendVpnServiceForm = new FormGroup({
+      'name': new FormControl(null),
+      'adminState': new FormControl(null),
+      'description': new FormControl(null)
+      });
+
+    this.addVpnServiceForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'subnet_id': new FormControl(null, [Validators.required]),
+      'router_id': new FormControl(null, [Validators.required]),
+      'availability_zone': new FormControl(null, [Validators.required])
+      });
+
+    this.amendIpsecPolicyForm = new FormGroup({
+      'name': new FormControl(null),
+      'encryption_algorithm': new FormControl(null),
+      'pfs': new FormControl(null),
+      'value': new FormControl(null),
+      'description': new FormControl(null)
+      });
+
+    this.addIpsecPolicyForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'transform_protocol': new FormControl(null, [Validators.required]),
+      'auth_algorithm': new FormControl(null, [Validators.required]),
+      'encapsulation_mode': new FormControl(null, [Validators.required]),
+      'encryption_algorithm': new FormControl(null, [Validators.required]),
+      'pfs': new FormControl(null, [Validators.required]),
+      'value': new FormControl(null, [Validators.required]),
+      'availability_zone': new FormControl(null, [Validators.required])
+      });
+
+    this.amendIkePolicyForm = new FormGroup({
+      'name': new FormControl(null),
+      'adminState': new FormControl(null),
+      'description': new FormControl(null)
+      });
+
+    this.addIkePolicyForm = new FormGroup({
+      'name': new FormControl(null),
+      'adminState': new FormControl(null),
+      'description': new FormControl(null)
+      });
+
+    this.amendConnectionForm = new FormGroup({
+      'psk': new FormControl(null),
+      'peer_cidrs': new FormControl(null),
+      'peer_address': new FormControl(null),
+      'peer_id': new FormControl(null),
+      'name': new FormControl(null),
+      'description': new FormControl(null)
+      });
+    
+    this.addConnectionForm = new FormGroup({
+      'psk': new FormControl(null, [Validators.required]),
+      'ipsecpolicy_id': new FormControl(null, [Validators.required]),
+      'peer_cidrs': new FormControl(null, [Validators.required]),
+      'ikepolicy_id': new FormControl(null, [Validators.required]),
+      'vpnservice_id': new FormControl(null, [Validators.required]),
+      'peer_address': new FormControl(null, [Validators.required]),
+      'peer_id': new FormControl(null, [Validators.required]),
+      'name': new FormControl(null, [Validators.required]),
+      'availability_zone': new FormControl(null, [Validators.required])
+      });
+
+      
   }
 
+  vpnChange(service) {
+    console.log(service);
+    this.ipsecvpnService.vpnServiceShow(this.currentProjectT, service.id);
+    this.ipsecvpnService.changeVpnService(service);
+  }
+
+  ipsecPolicyChange(policy) {
+    console.log(policy);
+    this.ipsecvpnService.ipsecPolicyShow(this.currentProjectT, policy.id);
+    this.ipsecvpnService.changeIpsecPolicy(policy);
+  }
+
+  ikePolicyChange(policy) {
+    console.log(policy);
+    this.ipsecvpnService.ikePolicyShow(this.currentProjectT, policy.id);
+    this.ipsecvpnService.changeIkePolicy(policy);
+  }
+
+  connectionChange(connection) {
+    console.log(connection);
+    this.ipsecvpnService.ipsecSiteConnectionShow(this.currentProjectT, connection.id);
+    this.ipsecvpnService.changeIpsecConnection(connection);
+  }
 }
